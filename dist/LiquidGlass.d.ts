6e3066720ce6835712485264a5a141f1a34aa27e
exist08 <askanuragsk08@gmail.com>
2026-05-18 12:23:58 +0530

    chore: remove dist from git tracking

--- dist/LiquidGlass.d.ts
+++
@@ -1,3 +0,0 @@
-import type { LiquidGlassProps } from "./types";
-export declare function LiquidGlass(props: LiquidGlassProps): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=LiquidGlass.d.ts.map
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { LiquidGlass } from "./LiquidGlass";
-export type { LiquidGlassProps, GlassVariant, NoiseType } from "./types";
-//# sourceMappingURL=index.d.ts.map
